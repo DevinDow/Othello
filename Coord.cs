02a57976d87fcd2b4ce05e7054a7a7096972102a
Devin Dow <DevinDow@gmail.com>
2023-09-22 02:20:44 -0700

    pulled BoardState & Coord classes out of Board.cs file

---
+++ Coord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Othello
+{
+    /// <summary>
+    /// Coord of Squares ( 1..8 , 1..8 )
+    /// </summary>
+    public class Coord
+    {
+        public int x, y;
+
+        public Coord()
+        {
+            x = -1;
+            y = -1;
+        }
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", x, y);
+        }
+    }
+}
