d9cb2dfac7009ddfc1782c807bd1ed24cc380ce6
Devin Dow <DevinDow@gmail.com>
2023-09-23 06:29:27 -0700

    ComputerPlayerTests.TestFirstMove()

---
+++ UnitTests/ComputerPlayerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Othello;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ComputerPlayerTests
+    {
+        [TestMethod]
+        public void TestFirstMove()
+        {
+            BoardState boardState = new BoardState();
+            ComputerPlayer computerPlayer = new ComputerPlayer();
+            computerPlayer.Level = LevelEnum.Beginner;
+            computerPlayer.BoardState = boardState;
+            Coord choice = computerPlayer.ChooseNextMove();
+            //Assert.AreEqual(new Coord(6, 3), choice);
+        }
+    }
+}
