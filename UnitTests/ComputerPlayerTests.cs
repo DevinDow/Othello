5a52c0c036dc910aa71c29f7465c1cc3fe738ad5
Devin Dow <DevinDow@gmail.com>
2023-09-23 07:09:24 -0700

    TestAdvancedFirstMove()

--- UnitTests/ComputerPlayerTests.cs
+++ UnitTests/ComputerPlayerTests.cs
@@ -25,5 +25,23 @@ namespace UnitTests
             };
             Assert.IsTrue(acceptableChoices.Contains(choice));
         }
+
+        [TestMethod]
+        public void TestAdvancedFirstMove()
+        {
+            BoardState boardState = new BoardState();
+            ComputerPlayer computerPlayer = new ComputerPlayer();
+            computerPlayer.Level = LevelEnum.Advanced;
+            computerPlayer.BoardState = boardState;
+            Coord choice = computerPlayer.ChooseNextMove();
+            List<Coord> acceptableChoices = new List<Coord>
+            {
+                new Coord(3,5),
+                new Coord(4,6),
+                new Coord(5,3),
+                new Coord(6,4),
+            };
+            Assert.IsTrue(acceptableChoices.Contains(choice));
+        }
     }
 }
