a4d58d38ecf1a54c4256d60c67002c30ea0265fe
Devin Dow <DevinDow@gmail.com>
2023-09-26 16:48:45 -0700

    ComputerPlayersCompete

---
+++ UnitTests/ComputerPlayersCompete.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Othello;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ComputerPlayersCompete
+    {
+        [TestMethod]
+        public void BegVsInt()
+        {
+            BoardState boardState = new BoardState();
+            ComputerPlayer blackBeg = new ComputerPlayer(LevelEnum.Beginner, false);
+            ComputerPlayer whiteInt = new ComputerPlayer(LevelEnum.Intermediate, true);
+
+            while (true)
+            {
+                ComputerPlayer currentPlayer = boardState.WhitesTurn ? whiteInt : blackBeg;
+                currentPlayer.BoardState = boardState;
+                Coord? choice = currentPlayer.ChooseNextMove();
+                if (choice != null)
+                    boardState.PlacePieceAndFlipPiecesAndChangeTurns(choice.Value);
+                else if (boardState.endOfGame)
+                    break;
+            }
+        }
+    }
+}
