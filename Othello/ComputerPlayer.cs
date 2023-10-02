21d46aff134ccbb90aa48705b9049558225f43be
Devin Dow <DevinDow@gmail.com>
2023-10-02 09:07:56 -0700

    trying 10,000 for corner, -1,000 for next to corner

--- Othello/ComputerPlayer.cs
+++ Othello/ComputerPlayer.cs
@@ -448,12 +448,22 @@ namespace Othello
                 default:
 					return 1;
 
-                // Higher Levels value Corners highest, then Ends.  It devalues Squares before Corners & Ends since they lead to Opponent getting Corners & Ends.
-                // 50  2 10 10
-                //  2  1  3  3
-                // 10  3 16  8
-                // 10  3  8  5
-				case LevelEnum.Intermediate:
+                    // Higher Levels value Corners highest, then Ends.  It devalues Squares before Corners & Ends since they lead to Opponent getting Corners & Ends.
+                    // 50  2 10 10
+                    //  2  1  3  3
+                    // 10  3 16  8
+                    // 10  3  8  5
+/*
+  [10000, -1000, 100, 100, 100, 100, -1000, 10000],
+  [-1000, -1000,   1,   1,   1,   1, -1000, -1000],
+  [  100,     1,  50,  50,  50,  50,     1,   100],
+  [  100,     1,  50,   1,   1,  50,     1,   100],
+  [  100,     1,  50,   1,   1,  50,     1,   100],
+  [  100,     1,  50,  50,  50,  50,     1,   100],
+  [-1000, -1000,   1,   1,   1,   1, -1000, -1000],
+  [10000, -1000, 100, 100, 100, 100, -1000, 10000],
+*/
+                case LevelEnum.Intermediate:
                 case LevelEnum.Advanced:
                 case LevelEnum.Expert:
                     switch (coord.x)
@@ -464,12 +474,12 @@ namespace Othello
 							{
 								case 1:
 								case 8:
-									return 50;
+									return 10000;
 								case 2:
 								case 7:
-									return 2;
+									return -1000;
 								default:
-									return 10;
+									return 100;
 							}
 						case 2:
 						case 7:
@@ -477,12 +487,12 @@ namespace Othello
 							{
 								case 1:
 								case 8:
-									return 2;
+									return -1000;
 								case 2:
 								case 7:
-									return 1;
+									return -1000;
 								default:
-									return 3;
+									return 1;
 							}
 						case 3:
 						case 6:
@@ -490,30 +500,30 @@ namespace Othello
 							{
 								case 1:
 								case 8:
-									return 10;
+									return 100;
 								case 2:
 								case 7:
-									return 3;
+									return 1;
 								case 3:
 								case 6:
-									return 16;
+									return 50;
 								default:
-									return 8;
+									return 50;
 							}
 						default:
 							switch (coord.y)
 							{
 								case 1:
 								case 8:
-									return 10;
+									return 100;
 								case 2:
 								case 7:
-									return 3;
+									return 1;
 								case 3:
 								case 6:
-									return 8;
+									return 50;
 								default:
-									return 5;
+									return 1;
 							}
 					}
 			}
