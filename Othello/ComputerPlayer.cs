cf04cfe366f858182d8c203aa6035b798b1df50f
Devin Dow <DevinDow@gmail.com>
2023-10-03 05:32:53 -0700

    Intermediate brings back Negatives in WeightedCoordValue() because it performs better with them helping make better decisions.  Advanced & Expert perform worse when Negatives throw a wrench in comparing BoardStates.

--- Othello/ComputerPlayer.cs
+++ Othello/ComputerPlayer.cs
@@ -441,22 +441,91 @@ namespace Othello
         /// <returns>weighted Score of coord</returns>
         private int WeightedCoordValue(Coord coord)
 		{
-			switch (Level)
+            // Beginner Level scores each square as 1
+            // Higher Levels value Coords differently
+            // Corners are highest, then Ends.
+            // Coords before Corners & Ends are devalued since they lead to Opponent getting Corners & Ends.
+            // And Coords before those are valuable since they lead to Opponent getting those devalued Coords.
+            switch (Level)
 			{
-                // Beginner Level scores each square as 1
                 case LevelEnum.Beginner:
                 default:
-					return 1;
+					return 1; // All Coords
 
-                // Higher Levels value Coords differently
-                // Corners are highest, then Ends.
-                // Coords before Corners & Ends are devalued since they lead to Opponent getting Corners & Ends.
-                // And Coords before those are valuable since they lead to Opponent getting those devalued Coords.
+                // 50 -5 20 20
+                // -5 -9 -2 -2
+                // 20 -2  4  2
+                // 20 -2  2  1
+                case LevelEnum.Intermediate:
+                    switch (coord.x)
+                    {
+                        // Edge COLs
+                        case 1:
+                        case 8:
+                            switch (coord.y)
+                            {
+                                case 1:
+                                case 8:
+                                    return 50; // Corner
+                                case 2:
+                                case 7:
+                                    return -5; // leads to Corner
+                                default:
+                                    return 20; // Edge
+                            }
+                        // COL before Edge
+                        case 2:
+                        case 7:
+                            switch (coord.y)
+                            {
+                                case 1:
+                                case 8:
+                                    return -5; // leads to Corner
+                                case 2:
+                                case 7:
+                                    return -9; // leads to Corner
+                                default:
+                                    return -2; // leads to Edge
+                            }
+                        // COL before COL before Edge
+                        case 3:
+                        case 6:
+                            switch (coord.y)
+                            {
+                                case 1:
+                                case 8:
+                                    return 20; // Edge
+                                case 2:
+                                case 7:
+                                    return -2; // leads to Edge
+                                case 3:
+                                case 6:
+                                    return 4; // leads to Opponent getting devalued Coord near Corner
+                                default:
+                                    return 2; // leads to Opponent getting devalued Coord near Edge
+                            }
+                        // middle COLs
+                        default:
+                            switch (coord.y)
+                            {
+                                case 1:
+                                case 8:
+                                    return 10; // Edge
+                                case 2:
+                                case 7:
+                                    return -2; // leads to Edge
+                                case 3:
+                                case 6:
+                                    return 2; // leads to Opponent getting devalued Coord near Edge
+                                default:
+                                    return 1; // Middles
+                            }
+                    }
+
                 // 1000   0 100 100
                 //    0   0   1   1
                 //  100   1  50  20
                 //  100   1  20   2
-                case LevelEnum.Intermediate:
                 case LevelEnum.Advanced:
                 case LevelEnum.Expert:
                     switch (coord.x)
