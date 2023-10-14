44d33a7db684bb5c1dfedf01e2f2d2d279a0f0da
Devin Dow <DevinDow@gmail.com>
2023-10-14 08:11:52 -0700

    Console, incl. ComputerPlayers.OutputToConsole

--- ComputerPlayers/Program.cs
+++ ComputerPlayers/Program.cs
@@ -2,6 +2,7 @@
 using OthelloLib;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace ComputerVsComputer
@@ -10,10 +11,17 @@ namespace ComputerVsComputer
     {
         static void Main(string[] args)
         {
+            ComputerPlayers.OutputToConsole = true;
+
             ComputerPlayer black = new ComputerPlayer_Advanced(false);
             ComputerPlayer white = new ComputerPlayer_Ultimate(true);
 
-            ComputerPlayers.ComputerVsComputer(black, white);
+            Console.WriteLine("{0}  vs.  {1}", black, white);
+            BoardState boardState = ComputerPlayers.ComputerVsComputer(black, white);
+            Console.WriteLine(boardState.ToString());
+            Console.WriteLine("Black={0} White={1}", boardState.BlackCount, boardState.WhiteCount);
+            Console.WriteLine("{0}  vs.  {1}", black, white);
+            Console.ReadKey();
         }
     }
 }
--- OthelloLib/ComputerPlayers.cs
+++ OthelloLib/ComputerPlayers.cs
@@ -8,6 +8,8 @@ namespace OthelloLib
 {
     public static class ComputerPlayers
     {
+        public static bool OutputToConsole = false;
+
         /// <summary>
         /// run a Game of black vs white
         /// </summary>
@@ -20,6 +22,8 @@ namespace OthelloLib
 
             while (true) // repeat currentPlayer making a Move until endOfGame
             {
+                DateTime start = DateTime.Now;
+
                 // which Player's turn is it (boardState.WhitesTurn knows if a Player is skipped due to no Legal Moves)
                 ComputerPlayer currentPlayer = boardState.WhitesTurn ? white : black;
                 Coord? choice = currentPlayer.ChooseNextMove(boardState);
@@ -28,6 +32,12 @@ namespace OthelloLib
                     boardState.PlacePieceAndFlipPiecesAndChangeTurns(choice.Value);
                     //Debug.Print(boardState.ToString()); // log BoardState after every Move
                     //Debug.Print("Black={0} White={1}", boardState.BlackCount, boardState.WhiteCount);
+                }
+
+                if (OutputToConsole)
+                {
+                    string lead = string.Format("{0:+#;-#;+0}", boardState.BlackCount - boardState.WhiteCount).PadLeft(3);
+                    Console.WriteLine("{0} : {1} remaining, {2:N1}\" by {3}", lead, boardState.EmptyCount, (DateTime.Now - start).TotalSeconds, currentPlayer);
                 }
 
                 // break when End Of Game
